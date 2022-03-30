a999f0c2ab1e9d75acaf12982937e6757cd90355
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2022-03-30 23:07:07 +0100

    Create Road Repair.py

---
+++ Road Repair.py
@@ -0,0 +1,29 @@
+def getMinCost(crew_id, job_id):
+    # Write your code here
+    crew_id.sort()
+    job_id.sort()
+    return sum(abs(cid - jid) for cid, jid in zip(crew_id, job_id))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    crew_id_count = int(input().strip())
+
+    crew_id = []
+
+    for _ in range(crew_id_count):
+        crew_id_item = int(input().strip())
+        crew_id.append(crew_id_item)
+
+    job_id_count = int(input().strip())
+
+    job_id = []
+
+    for _ in range(job_id_count):
+        job_id_item = int(input().strip())
+        job_id.append(job_id_item)
+
+    result = getMinCost(crew_id, job_id)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
